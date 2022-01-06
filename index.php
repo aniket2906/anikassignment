3bb854338e7326491698f25e72574c73669b2fe3
Anikets2906 <anikets2906@gmail.com>
2022-01-06 08:51:40 +0400

    card layout

--- index.php
+++ index.php
@@ -13,7 +13,9 @@
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
 </head>
 <body>
-    <h1>Hello World</h1>
+    <div class="card">
+        <div class="container">
+            <h1>Available Balance</h1>
 </body>
 </html>
 
